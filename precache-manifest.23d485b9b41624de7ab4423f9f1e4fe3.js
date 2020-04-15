a57b04c86612273b507bd7d8d8c36ea8645ade8a
kunal-motiani <kunal_motiani2002@yahoo.com>
2020-04-15 17:13:27 +0530

    Updates

---
+++ precache-manifest.23d485b9b41624de7ab4423f9f1e4fe3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a626385a5425dd0f1308f4c169f853c",
+    "url": "/animal-sound/index.html"
+  },
+  {
+    "revision": "10bc105a615d6aa85fcf",
+    "url": "/animal-sound/static/css/main.8991813b.chunk.css"
+  },
+  {
+    "revision": "c283bef393b57e2ba35f",
+    "url": "/animal-sound/static/js/2.3b8ffeca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/animal-sound/static/js/2.3b8ffeca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10bc105a615d6aa85fcf",
+    "url": "/animal-sound/static/js/main.c6b62937.chunk.js"
+  },
+  {
+    "revision": "f117f989e003d962622b",
+    "url": "/animal-sound/static/js/runtime-main.b514f703.js"
+  }
+]);
